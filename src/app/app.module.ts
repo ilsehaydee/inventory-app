cdac1ba7b19e77004b26e1dbbaeb0d491447a236
Ilse Haydee Gonzalez Arechiga <ilse.by.ok@live.com.mx>
2021-11-03 18:20:54 -0600

    added product list component, product row component and image ccomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductRowComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
